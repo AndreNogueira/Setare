28ed2f9ac1efab6ad1ff20af7a5265151ac4fe7b
André Nogueira <andre.nogueira26@gmail.com>
2014-03-13 16:57:05 +0000

    Migration City. Criada a relação entre city e country

---
+++ db/migrate/20140313165136_create_cities.rb
@@ -0,0 +1,13 @@
+class CreateCities < ActiveRecord::Migration
+  def change
+    create_table :cities do |t|
+      t.string :name, null: false
+      t.boolean :is_taxi, null: false, default: false
+
+      t.integer :country_id, null: false
+      t.foreign_key :countries, dependent: :destroy
+
+      t.timestamps
+    end
+  end
+end
