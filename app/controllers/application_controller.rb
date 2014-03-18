18270fab0c6b24c935dd02583a9dd1095f93a8a9
André Nogueira <andre.nogueira26@gmail.com>
2014-03-18 14:12:04 +0000

    Adicionado página de inicio mais as ligaçoes para as paginas de perfil e logout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_devise_params, if: :devise_controller?
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :phone_number, :driver_license,:nif,:bi,:address)
+    end
+    #devise_parameter_sanitizer.for(:account_update) do |u|
+     # u.permit(:name, :username, :email, :birth_date, :avatar, :password, :password_confirmation)
+    #end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Setare::Application.routes.draw do
-  devise_for :users
+
+
+
+  devise_for :users, :controllers => {:registrations => 'registrations'}
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
