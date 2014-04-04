8472c343f68b2e5eb2a566b29336d06424ea305a
André Nogueira <andre.nogueira26@gmail.com>
2014-04-04 15:01:11 +0100

    Melhorado código para a criação de pick ups locations de taxis. Já funciona mais rápido.

--- db/seeds/pick_up_locations_taxi.seeds.rb
+++ db/seeds/pick_up_locations_taxi.seeds.rb
@@ -1,25 +1,16 @@
 after :taxis, :pick_up_locations do
 
+  puts '--------- Begin of PickUpLocations for Taxis --------------'
   taxis     = Taxi.all
-  locations = PickUpLocation.all.reorder("id asc")
+  locations = PickUpLocation.all
 
-
-  taxis.each do |f|
-    puts 'Begin ----   PickUpLocations para o carro '+f.name.to_s
-    ActiveRecord::Base.transaction do
-      pickUpLocations = []
-      picks_random    = []
-      (0..580).each do
-        pick_id = rand(0..6960)
-        while picks_random.include?(pick_id)
-          pick_id = rand(0..6960)
-        end
-        pickUpLocations << locations[pick_id]
-      end
-
-      f.pick_up_locations << pickUpLocations
-      puts 'End   ----   PickUpLocations para o carro '+f.name.to_s + '\n'
+  Taxi.transaction do
+    taxis.each do |taxi|
+      puts 'Taxi '+ taxi.name.to_s
+      taxi.pick_up_locations << locations.sample(600)
     end
   end
+  puts '--------- End of PickUpLocations for Taxis --------------'
+
 
 end
