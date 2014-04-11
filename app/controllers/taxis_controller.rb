56be593342babfd03a8d211615da1fd6ba9c57d6
André Nogueira <andre.nogueira26@gmail.com>
2014-04-11 14:09:28 +0100

    Eliminação de código repetido no controller dos Taxis

--- app/controllers/taxis_controller.rb
+++ app/controllers/taxis_controller.rb
@@ -1,27 +1,21 @@
 class TaxisController < ApplicationController
+
+  respond_to :html, :json
+
   def index
-    @countries = Country.taxis
+    respond_with(@countries = Country.taxis)
   end
 
   def city
-    cities = City.taxis(params[:id])
-    respond_to do |format|
-      format.json { render :json => cities}
-    end
+    respond_with(City.taxis(params[:id]))
   end
 
   def pick_up
-    pick_ups = PickUpLocation.all_pick_ups_from_city(params[:id])
-    respond_to do |format|
-      format.json { render :json => pick_ups}
-    end
+    respond_with(PickUpLocation.all_pick_ups_from_city(params[:id]))
   end
 
   def drop_off
-    drop_offs = PickUpLocation.drop_offs_from_pick_up(params[:id])
-    respond_to do |format|
-      format.json { render :json => drop_offs}
-    end
+    respond_with(PickUpLocation.drop_offs_from_pick_up(params[:id]))
   end
 
   def search_results
