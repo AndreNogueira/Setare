3bf7086ad4791e68f736032b2a6c27230e80930c
André Nogueira <andre.nogueira26@gmail.com>
2014-04-23 14:51:45 +0100

    Finalizado a construção do template de email a enviar no caso de ser uma reserva de taxi.

--- app/mailers/taxi_reservation_mailer.rb
+++ app/mailers/taxi_reservation_mailer.rb
@@ -1,8 +1,10 @@
 class TaxiReservationMailer < ActionMailer::Base
 
+  helper ActionView::Helpers::NumberHelper
   def taxi_reservation(taxi_service, user)
-
-
+    @user = user
+    @reservation = taxi_service
+    #attachments['setare-logo.png'] = File.read("#{Rails.root}/app/assets/images/setare-logo.png")
     mail(to: 'andre.nogueira26@gmail.com', from: 'noreply.setare@gmail.com',subject: "[Setare]: Taxi Reservation nº #{taxi_service.id}")
   end
 end
