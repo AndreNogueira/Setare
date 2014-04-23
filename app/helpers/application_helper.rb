b3fefe94b661dde1e23e51623d1bd052dd5faa1a
André Nogueira <andre.nogueira26@gmail.com>
2014-04-23 16:19:15 +0100

    Adicionado um partial e um helper para mostrar as mensagens de flash de erros transformadas para usar as alert-boxs do foundation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,16 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  def flash_class(level)
+    case level
+      when :info then
+        'alert-box info'
+      when :success then
+        'alert-box success'
+      when :warning then
+        'alert-box warning'
+      else
+        'alert-box'
+    end
+  end
 end
