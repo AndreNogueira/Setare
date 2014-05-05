5548dae580a4c0d716d6196e031803b586d42256
pjmaia <pjmaia10@gmail.com>
2014-05-05 16:16:28 +0100

    Imagens da App;  Models e helpers adicionados

---
+++ app/controllers/car_controller.rb
@@ -0,0 +1,78 @@
+class CarController < ApplicationController
+  def index
+    @countries = Country.with_subsidiaries
+  end
+
+  def pick_city
+    json_respond(City.with_subsidiaries(params[:id]))
+  end
+
+  def pick_subsidiary
+    json_respond(Subsidiary.where(city_id: params[:id]))
+  end
+
+  def drop_city
+    json_respond(City.cities_with_same_agency(params[:country_id],params[:agency_id]))
+  end
+
+  def drop_subsidiary
+    @d_subs = Subsidiary.drop_subsidiaries(params[:city_id], params[:agency_id])
+    json_respond(@d_subs)
+  end
+
+  def get_agency
+    agency = Agency.joins(:subsidiaries).where(subsidiaries: {id: params[:id]})
+    json_respond(agency)
+  end
+
+  def search_results
+    #save Search fields
+    session[:service_params] = params
+    if params[:return_at_same_location]
+      session[:service_params]["drop_city"] = params[:pick_city]
+      session[:service_params]["drop_subsidiary"] = params[:pick_subsidiary]
+    end
+    @locations = CarLocations.new(session[:service_params])
+    #list of all Available cars in the choosen subsidiary
+    @list_selected_subsidiary = Car.check_available_cars(params[:pick_subsidiary],params[:begin_date],params[:end_date])
+    remaining_subsidiaries = Subsidiary.other_subsidiaries(params[:pick_city],session[:service_params]["drop_city"],params[:pick_subsidiary])
+    #list of all Available cars in others subsidiaries from the same cities
+    @other_agencies_list = []
+    remaining_subsidiaries.each do |f|
+      @other_agencies_list << Car.check_available_cars(f,params[:begin_date],params[:end_date])
+    end
+  end
+
+  def service_extras
+    session[:service_params]["car_id"] = params[:car_id]
+    @locations = CarLocations.new(session[:service_params])
+  end
+
+  def service_reservation
+    payment = CarPayment.new(payment_params: params)
+    result = payment.payment_status
+    if result[:service]
+      flash[:success] = result[:message]
+    else
+      flash.now[:warning] = result[:message]
+      render 'car/service_payment'
+    end
+  end
+
+  def service_payment
+    if !params.nil?
+      @car_extras = CarExtras.new(params,session[:service_params])
+      session[:service_params]["final_price"] = @car_extras.total_price
+      session[:service_params]["extras"] = @car_extras.extras
+      session[:service_params]["extras_price"] = @car_extras.extras_price
+    end
+  end
+
+
+  private
+  def json_respond(parameter)
+    respond_to do |format|
+      format.json { render :json => parameter}
+    end
+  end
+end
---
+++ app/models/car_locations.rb
@@ -0,0 +1,33 @@
+class CarLocations
+  include ActiveAttr::Model
+
+  attribute :pick_country, type: Integer
+  attribute :pick_city, type: Integer
+  attribute :pick_subsidiary,type: Integer
+  attribute :drop_city,type: Integer
+
+  attr_reader :countries,
+              :pick_cities,
+              :pick_subs,
+              :drop_cities,
+              :drop_subs
+
+
+  def initialize(attributes)
+    super
+    @countries = Country.with_subsidiaries
+    @pick_cities = City.with_subsidiaries(pick_country)
+    @pick_subs = Subsidiary.where(city_id: pick_city)
+    @drop_cities = City.cities_with_same_agency(pick_country, Subsidiary.find(pick_subsidiary).agency.id)
+    @drop_subs = Subsidiary.drop_subsidiaries(check_params,Subsidiary.find(pick_subsidiary).agency.id)
+  end
+
+  private
+  def check_params
+    if drop_city == 0
+      drop_city = pick_city
+    else
+      drop_city
+    end
+  end
+end
