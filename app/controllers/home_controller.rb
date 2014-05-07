6f5bca37ed2619ac6ac9bbb6be518dba497b250d
pjmaia <pjmaia10@gmail.com>
2014-05-07 22:27:03 +0100

    Feita A Listagem dos CarServices.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
 
-  before_filter :authenticate_user!, only: [:reservations]
+  before_filter :authenticate_user!, only: [:reservations,:car_reservations]
 
   def index
   end
 
   def reservations
     @taxis = current_user.taxi_services
+    @cars = current_user.car_services.includes(:car)
   end
 end
