e3f89be72b9e25103b449fee8e9348cfdbdf363c
pjmaia <pjmaia10@gmail.com>
2014-05-13 15:48:25 +0100

    Refactor do Service Mail CarService

--- app/mailers/car_reservation_mailer.rb
+++ app/mailers/car_reservation_mailer.rb
@@ -5,6 +5,11 @@ class CarReservationMailer < ActionMailer::Base
   def car_reservation(car_service, user)
     @user        = user
     @reservation = car_service
+    @pick_up = {pick_up_sub: Subsidiary.find(car_service.pick_up_subsidiary_id).name,
+                pick_up_city: Subsidiary.find(car_service.pick_up_subsidiary_id).city.name}
+    @drop_off = {drop_off_sub: Subsidiary.find(car_service.drop_off_subsidiary_id).name,
+                 drop_off_city: Subsidiary.find(car_service.drop_off_subsidiary_id).city.name}
+
     mail(to: user.email, from: 'noreply.setare@gmail.com', subject: "[Setare]: Car Reservation nº #{car_service.id}")
   end
 end
