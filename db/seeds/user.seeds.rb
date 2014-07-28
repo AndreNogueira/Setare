ce6b35e129c9bbe1f265e4d5bdf4272e7508a64e
André Nogueira <andre.nogueira26@gmail.com>
2014-07-28 16:05:38 +0100

    Alterado o email do utilizador teste.

--- db/seeds/user.seeds.rb
+++ db/seeds/user.seeds.rb
@@ -1,3 +1,3 @@
 after :user_profiles do
-  User.create(name:'jose esteves', email:'pjmaia10@gmail.com',bi:'123123123',nif:'123123123',address:'Rua do esgoto',password:"12345678",driver_license:'P-3213213',phone_number:'912311234')
+  User.create(name:'jose esteves', email:'noreply.setare@gmail.com',bi:'123123123',nif:'123123123',address:'Rua do esgoto',password:"12345678",driver_license:'P-3213213',phone_number:'912311234')
 end
