ba7738b87118fbb2b3b0e0b494e2dac82344ca48
André Nogueira <andre.nogueira26@gmail.com>
2014-07-30 14:55:16 +0100

    Corrigido erro criado por informação errada.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 Setare::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.thread_safe!
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
